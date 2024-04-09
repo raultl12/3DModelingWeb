dfdd94a0d2cb844225930f95f4b997ab8a79ae4c
Raul <raultl6212@gmail.com>
2024-04-09 18:47:50 +0200

    Cambios menores

--- src/objects/Light.js
+++ src/objects/Light.js
@@ -11,29 +11,31 @@ class Light {
             case LightType.POINT:
                 this.light = new THREE.PointLight(color, intensity);
                 this.helper = new THREE.PointLightHelper( this.light, 1 );
-                scene.add( this.helper );
+                
                 break;
             case LightType.DIRECTIONAL:
                 this.light = new THREE.DirectionalLight(color, intensity);
-
                 this.helper = new THREE.DirectionalLightHelper( this.light, 5 );
-                scene.add( this.helper );
+
+                
                 break;
             case LightType.SPOT:
                 this.light = new THREE.SpotLight(color, intensity);
                 this.helper = new THREE.SpotLightHelper( this.light );
 
-                scene.add( this.helper );
+                
                 break;
             default:
                 this.light = new THREE.PointLight(color, intensity);
                 this.helper = new THREE.PointLightHelper( this.light, 1 );
-                scene.add( this.helper );
+                
                 break;
         }
+        
         this.light.position.set(0, 0, 0);
         
         this.helper.name = "light";
+        scene.add(this.helper);
 
         scene.add(this.light);
     }
